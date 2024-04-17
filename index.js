b57b9743a17cc5706aeab19fa25ee05811301956
Op-Hecker <118012705+Op-Hecker@users.noreply.github.com>
2024-04-17 14:27:50 +0500

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["🤍 Listening to Hecker#7260","📊 Watching 2 Servers","🎵 Listening to Brown Munde"];
+const statusMessages = ["🤍 Listening to Hecker#7260","📊 Watching 2 Servers","🎵 Listening to **ALORS BRAZIL**"];
 
 
 let currentIndex = 0;
@@ -98,7 +98,7 @@ client.once('ready', () => {
 
   setInterval(() => {
     updateStatusAndSendMessages();
-  }, 50000);
+  }, 5000);
 });
 
 login();
